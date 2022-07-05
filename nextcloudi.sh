d630fd80ec6d062d8be7f983bcdf616f2a8fe844
YoAmo <59877274+YoAmo@users.noreply.github.com>
2022-07-05 21:12:09 +0200

    Create nextcloudi.sh

---
+++ nextcloudi.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt install apache2 mariadb-server libapache2-mod-php -y && sudo apt install php-gd php-json php-mysql php-curl php-mbstring php-intl php-imagick php-xml php-zip -y && cd /var/www/html && sudo wget https://download.nextcloud.com/server/releases/latest-23.tar.bz2 && sudo tar -jxvf latest-23.tar.bz2
+sudo chmod 750 nextcloud -R
+sudo chown www-data:www-data nextcloud
+sudo reboot
